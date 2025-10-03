63f7156a19c6d84d3fecdb7036e030710245e53a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-03 16:08:03 +0300

    Update custom_error_code_01.cpp

--- custom_error_code_01.cpp
+++ custom_error_code_01.cpp
@@ -2,3 +2,9 @@
 // bunun için 2 ayrı yol olabilir
 // error_code yolu: “Somut hata kaynağı” (OS, kütüphane, modül) temsil edilir. Bir category (türetilmiş std::error_category) gerekir.
 // error_condition yolu: “Taşınabilir, soyut sınıflandırma” (permission_denied, not_found vb.) için condition kategorisi ve default_error_condition eşlemesi yapılır.
+// Gerekli parçalar:
+// Hata sabitleri için bir enum / enum class.
+// Tekil (singleton) bir error_category türevi.
+// std::make_error_code(MyErr) (veya std::make_error_condition(MyCond)) overload’ı.
+// std::is_error_code_enum<MyErr> (veya std::is_error_condition_enum<MyCond>) trait explicit specializaton.
+// (opsiyonel) error_category::default_error_condition ile code→condition eşlemesi.
