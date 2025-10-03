ba960aa8a53775d7bdd0b11e04067c27cf0a1876
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-10-03 18:23:12 +0300

    Update constructors_02.cpp

--- constructors_02.cpp
+++ constructors_02.cpp
@@ -6,7 +6,7 @@ int main()
 
     FILE* f = std::fopen("nonexistent.txt", "r");
     if (!f) {
-        std::error_code ec(errno, std::generic_category()); /
+        std::error_code ec(errno, std::generic_category()); 
         std::cout << "File open failed: " << ec.message() << '\n';
 
         if (ec == std::errc::no_such_file_or_directory)
